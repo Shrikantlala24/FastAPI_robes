bd82deaebb0b3ecc51703b82024f5d1b0f138f70
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-04-26 00:35:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 import streamlit as st
 import requests
 
+# todo we'll create a simple app where we are taking inputs from the user and then sending it to the backend and then showing the response from the backend
