8d84839cb6733d78898e31c09fcbd848bbfb5272
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-05-13 16:08:15 +0530

    bmi - computed field

--- 6_Post/app.py
+++ 6_Post/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 import json
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, compute_field
 from typing import Annotated, Literal
 
 app = FastAPI()
@@ -17,15 +17,15 @@ class Patient(BaseModel) :
     ]
     city : Annotated [
         str,
-        Field(...,description='Enter the Patient's City')
+        Field(...,description='Enter the Patient City')
     ]
     age : Annotated [
         int,
-        Field(..., gt = 0, lt=120, description='Enter the Patient's Age')
+        Field(..., gt = 0, lt=120, description='Enter the Patient Age')
     ]
     gender: Annotated [
         Literal['male','female','others'],
-        Field(...,description='Enter the Patient's Gender')
+        Field(...,description='Enter the Patient Gender')
     ]
     height : Annotated [
         int,
@@ -36,25 +36,13 @@ class Patient(BaseModel) :
         Field(..., gt=0,description='Enter the Patient weight')
     ]
 
-
-
-
-
-
-
-
-
-
-    
-    # id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
-    # name: Annotated[str, Field(..., description='Name of the patient')]
-    # city: Annotated[str, Field(..., description='City where the patient is living')]
-    # age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
-    # gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
-    # height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
-    # weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
-
-
+    @compute_field
+    @property
+    def bmi(self) -> float:
+        return round(
+            self.weight/(self.height **2),
+            2
+        )
     
 def load_data():
     with open('../store.json', 'r') as f:
