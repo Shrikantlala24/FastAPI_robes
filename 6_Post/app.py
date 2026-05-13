1e51745345b5e3a6af192ebc484955dd58c25652
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-05-13 16:04:13 +0530

    model/schema ready

--- 6_Post/app.py
+++ 6_Post/app.py
@@ -1,11 +1,61 @@
 from fastapi import FastAPI, HTTPException
 import json
 
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, Field
+from typing import Annotated, Literal
 
 app = FastAPI()
 
+class Patient(BaseModel) :
+    pid : Annotated[
+        str,
+        Field(...,description='Enter the Patient ID', examples=['P001'])
+    ]
+    name : Annotated [
+        str,
+        Field(...,description='Enter the Patient name')
+    ]
+    city : Annotated [
+        str,
+        Field(...,description='Enter the Patient's City')
+    ]
+    age : Annotated [
+        int,
+        Field(..., gt = 0, lt=120, description='Enter the Patient's Age')
+    ]
+    gender: Annotated [
+        Literal['male','female','others'],
+        Field(...,description='Enter the Patient's Gender')
+    ]
+    height : Annotated [
+        int,
+        Field(..., gt=0, description='Enter the Patient hieght')
+    ]
+    weight : Annotated [
+        int,
+        Field(..., gt=0,description='Enter the Patient weight')
+    ]
+
+
+
+
+
+
+
+
+
+
+    
+    # id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    # name: Annotated[str, Field(..., description='Name of the patient')]
+    # city: Annotated[str, Field(..., description='City where the patient is living')]
+    # age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    # gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    # height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    # weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
+
+
+    
 def load_data():
     with open('../store.json', 'r') as f:
         return json.load(f)
